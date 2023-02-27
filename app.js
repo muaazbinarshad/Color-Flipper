3ad741eb2ec4566eaf4c99451f18b98ed9ca4a7f
muaazramay <62594813+muaazramay@users.noreply.github.com>
2023-02-27 14:55:23 +0500

    developed the logic

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const btn = document.getElementById('btn');
+const color = document.querySelector('.color');
+btn.addEventListener('click', function () {
+    randomNumber = Math.floor(Math.random() * colors.length);
+    console.log(randomNumber);
+    document.body.style.background = colors[randomNumber];
+    color.textContent = colors[randomNumber];
+})
+
+
+// backgroundCol = document.body.style.background;
+// backgroundCol = 'red';
